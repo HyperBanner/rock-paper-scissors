03ae0f5b65faa0a0dd1edff46f6b870f383556be
HyperBanner <131620724+HyperBanner@users.noreply.github.com>
2025-01-01 23:58:58 +0100

    getHumanChoice() function done.
    Figured out the logic for getting the human player's choice, added in
    error prevention and potential issues that may come up if the human
    player types in something incorrect.

--- script.js
+++ script.js
@@ -5,4 +5,21 @@ function getComputerChoice() {
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
-console.log(getComputerChoice());
+function getHumanChoice() {
+  let choice = String(prompt("Choose: Rock, Paper, Scissors")).toLowerCase();
+
+  if (choice === "null" || choice === "") {
+    alert("Please type in a valid option.");
+    getHumanChoice();
+  } else if (choice === "rock") {
+    return "rock";
+  } else if (choice === "paper") {
+    return "paper";
+  } else if (choice === "scissors") {
+    return "scissors";
+  } else {
+    alert("Please type in a valid option.");
+    getHumanChoice();
+  }
+}
+getHumanChoice();
