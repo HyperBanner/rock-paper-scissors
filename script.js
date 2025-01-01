36b5025ddcbf7a20fa96a9bd8b42d79336af511f
HyperBanner <131620724+HyperBanner@users.noreply.github.com>
2025-01-02 00:20:01 +0100

    Round logic figured out
    Added function playRound() which takes in the parameters of the human's
    choice and the computer's choice, and does a bunch of checks to see who
    wins in what scenario, writing line to the console to inform the human
    player who won, also increments the score variables based on who wins.
    
    Added variables to get the return of humanChoice and computerChoice to
    then pass into playRound().

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 "use strict";
 
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   let choices = ["rock", "paper", "scissors"];
   return choices[Math.floor(Math.random() * choices.length)];
@@ -22,4 +25,36 @@ function getHumanChoice() {
     getHumanChoice();
   }
 }
-getHumanChoice();
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === "rock" && computerChoice === "rock") {
+    console.log("It's a draw! Rock vs Rock");
+  } else if (humanChoice === "rock" && computerChoice === "paper") {
+    console.log("You lose! Paper beats Rock.");
+    computerScore++;
+  } else if (humanChoice === "rock" && computerChoice === "scissors") {
+    console.log("You win! Rock beats Scissors");
+    humanScore++;
+  } else if (humanChoice === "paper" && computerChoice === "rock") {
+    console.log("You win! Paper beats Rock!");
+    humanScore++;
+  } else if (humanChoice === "paper" && computerChoice === "paper") {
+    console.log("It's a draw! Paper vs Paper");
+  } else if (humanChoice === "paper" && computerChoice === "scissors") {
+    console.log("You lose! Scissors beat Paper.");
+    computerScore++;
+  } else if (humanChoice === "scissors" && computerChoice === "rock") {
+    console.log("You lose! Rock beats Paper.");
+    computerScore++;
+  } else if (humanChoice === "scissors" && computerChoice === "paper") {
+    console.log("You win! Scissors beat Paper.");
+    humanScore++;
+  } else {
+    console.log("It's a draw! Scissors vs Scissors");
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
