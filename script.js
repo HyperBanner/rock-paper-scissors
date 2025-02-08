4b3ea3f26b99281b2db2d2ed6350beb67dc2a259
HyperBanner <131620724+HyperBanner@users.noreply.github.com>
2025-02-08 16:02:17 +0100

    Round logic redone
    Replaced all console.logs with DOM methods,
    and fixed scores being reset with each playRound call.

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const btn_rock = document.querySelector(".btn_rock");
 const btn_paper = document.querySelector(".btn_paper");
 const btn_scissors = document.querySelector(".btn_scissors");
 
+let humanScore = 0;
+let computerScore = 0;
+
 btn_rock.addEventListener("click", () => {
   playRound("rock", getComputerChoice());
 });
@@ -19,60 +22,40 @@ function getComputerChoice() {
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
-function getHumanChoice() {
-  let choice = String(prompt("Choose: Rock, Paper, Scissors")).toLowerCase();
-
-  if (choice === "null" || choice === "") {
-    alert("Please type in a valid option.");
-    getHumanChoice();
-  } else if (choice === "rock") {
-    return "rock";
-  } else if (choice === "paper") {
-    return "paper";
-  } else if (choice === "scissors") {
-    return "scissors";
-  } else {
-    alert("Please type in a valid option.");
-    getHumanChoice();
-  }
-}
-
 function playRound(humanChoice, computerChoice) {
-  let humanScore = 0;
-  let computerScore = 0;
+  let message = document.querySelector(".score_message");
+  let human_score_display = document.querySelector(".player_score");
+  let computer_score_display = document.querySelector(".computer_score");
 
   if (humanChoice === "rock" && computerChoice === "rock") {
-    console.log("It's a draw! Rock vs Rock");
-    console.log(`Scores: You: ${humanScore} Computer: ${computerScore}`);
+    message.textContent = "It's a draw! Rock vs Rock";
   } else if (humanChoice === "rock" && computerChoice === "paper") {
-    console.log("You lose! Paper beats Rock.");
+    message.textContent = "You lose! Paper beats Rock.";
     computerScore++;
-    console.log(`Scores: You: ${humanScore} Computer: ${computerScore}`);
+    computer_score_display.textContent = `${computerScore}`;
   } else if (humanChoice === "rock" && computerChoice === "scissors") {
-    console.log("You win! Rock beats Scissors");
+    message.textContent = "You win! Rock beats Scissors";
     humanScore++;
-    console.log(`Scores: You: ${humanScore} Computer: ${computerScore}`);
+    human_score_display.textContent = `${humanScore}`;
   } else if (humanChoice === "paper" && computerChoice === "rock") {
-    console.log("You win! Paper beats Rock!");
+    message.textContent = "You win! Paper beats Rock!";
     humanScore++;
-    console.log(`Scores: You: ${humanScore} Computer: ${computerScore}`);
+    human_score_display.textContent = `${humanScore}`;
   } else if (humanChoice === "paper" && computerChoice === "paper") {
-    console.log("It's a draw! Paper vs Paper");
-    console.log(`Scores: You: ${humanScore} Computer: ${computerScore}`);
+    message.textContent = "It's a draw! Paper vs Paper";
   } else if (humanChoice === "paper" && computerChoice === "scissors") {
-    console.log("You lose! Scissors beat Paper.");
+    message.textContent = "You lose! Scissors beat Paper.";
     computerScore++;
-    console.log(`Scores: You: ${humanScore} Computer: ${computerScore}`);
+    computer_score_display.textContent = `${computerScore}`;
   } else if (humanChoice === "scissors" && computerChoice === "rock") {
-    console.log("You lose! Rock beats Paper.");
+    message.textContent = "You lose! Rock beats Paper.";
     computerScore++;
-    console.log(`Scores: You: ${humanScore} Computer: ${computerScore}`);
+    computer_score_display.textContent = `${computerScore}`;
   } else if (humanChoice === "scissors" && computerChoice === "paper") {
-    console.log("You win! Scissors beat Paper.");
+    message.textContent = "You win! Scissors beat Paper.";
     humanScore++;
-    console.log(`Scores: You: ${humanScore} Computer: ${computerScore}`);
+    human_score_display.textContent = `${humanScore}`;
   } else {
-    console.log("It's a draw! Scissors vs Scissors");
-    console.log(`Scores: You: ${humanScore} Computer: ${computerScore}`);
+    message.textContent = "It's a draw! Scissors vs Scissors";
   }
 }
