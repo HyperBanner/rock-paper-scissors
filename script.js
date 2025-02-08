3f1ee4f82330cd4da7c35e7f5bcb96c9204d33ae
HyperBanner <131620724+HyperBanner@users.noreply.github.com>
2025-02-08 16:34:19 +0100

    Project done (for now)
    Added the logic for checking if either player
    has at least 5 points, and if so, the game ends.
    Also cleaned up some variable names.

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 "use strict";
 
-const btn_rock = document.querySelector(".btn_rock");
-const btn_paper = document.querySelector(".btn_paper");
-const btn_scissors = document.querySelector(".btn_scissors");
+const main = document.querySelector(".main");
+const buttons = document.querySelector(".buttons");
+const btnRock = document.querySelector(".btn_rock");
+const btnPaper = document.querySelector(".btn_paper");
+const btnScissors = document.querySelector(".btn_scissors");
+const scoreDiv = document.querySelector(".score")
 
 let humanScore = 0;
 let computerScore = 0;
+let message = document.querySelector(".score_message");
 
-btn_rock.addEventListener("click", () => {
+btnRock.addEventListener("click", () => {
   playRound("rock", getComputerChoice());
+  checkResults();
 });
-btn_paper.addEventListener("click", () => {
+btnPaper.addEventListener("click", () => {
   playRound("paper", getComputerChoice());
+  checkResults();
 });
-btn_scissors.addEventListener("click", () => {
+btnScissors.addEventListener("click", () => {
   playRound("scissors", getComputerChoice());
+  checkResults();
 });
 
 function getComputerChoice() {
@@ -23,39 +30,56 @@ function getComputerChoice() {
 }
 
 function playRound(humanChoice, computerChoice) {
-  let message = document.querySelector(".score_message");
-  let human_score_display = document.querySelector(".player_score");
-  let computer_score_display = document.querySelector(".computer_score");
+  let humanScoreDisplay = document.querySelector(".player_score");
+  let computerScoreDisplay = document.querySelector(".computer_score");
 
   if (humanChoice === "rock" && computerChoice === "rock") {
     message.textContent = "It's a draw! Rock vs Rock";
   } else if (humanChoice === "rock" && computerChoice === "paper") {
     message.textContent = "You lose! Paper beats Rock.";
     computerScore++;
-    computer_score_display.textContent = `${computerScore}`;
+    computerScoreDisplay.textContent = `${computerScore}`;
   } else if (humanChoice === "rock" && computerChoice === "scissors") {
     message.textContent = "You win! Rock beats Scissors";
     humanScore++;
-    human_score_display.textContent = `${humanScore}`;
+    humanScoreDisplay.textContent = `${humanScore}`;
   } else if (humanChoice === "paper" && computerChoice === "rock") {
     message.textContent = "You win! Paper beats Rock!";
     humanScore++;
-    human_score_display.textContent = `${humanScore}`;
+    humanScoreDisplay.textContent = `${humanScore}`;
   } else if (humanChoice === "paper" && computerChoice === "paper") {
     message.textContent = "It's a draw! Paper vs Paper";
   } else if (humanChoice === "paper" && computerChoice === "scissors") {
     message.textContent = "You lose! Scissors beat Paper.";
     computerScore++;
-    computer_score_display.textContent = `${computerScore}`;
+    computerScoreDisplay.textContent = `${computerScore}`;
   } else if (humanChoice === "scissors" && computerChoice === "rock") {
     message.textContent = "You lose! Rock beats Paper.";
     computerScore++;
-    computer_score_display.textContent = `${computerScore}`;
+    computerScoreDisplay.textContent = `${computerScore}`;
   } else if (humanChoice === "scissors" && computerChoice === "paper") {
     message.textContent = "You win! Scissors beat Paper.";
     humanScore++;
-    human_score_display.textContent = `${humanScore}`;
+    humanScoreDisplay.textContent = `${humanScore}`;
   } else {
     message.textContent = "It's a draw! Scissors vs Scissors";
+  }
+}
+
+function checkResults() {
+  if (humanScore === 5 || computerScore === 5) {
+    endGame();
+  }
+}
+
+function endGame() {
+  main.removeChild(buttons);
+  let endMessage = document.querySelector(".end_message");
+  if (humanScore > computerScore) {
+    endMessage.textContent = "You won the game!";
+    scoreDiv.removeChild(message);
+  } else {
+    endMessage.textContent = "You lost the game!";
+    scoreDiv.removeChild(message);
   }
 }
