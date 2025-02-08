9a8428ec20c687068a92abb06fd0ce63915ecfbb
HyperBanner <131620724+HyperBanner@users.noreply.github.com>
2025-02-08 14:40:50 +0100

    Buttons work
    Added event listeners to each button,
    each event listener calls the playRound function
    with the right arguments, playGame function removed,
    alongside with other redundant logic.

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 "use strict";
 
+const btn_rock = document.querySelector(".btn_rock");
+const btn_paper = document.querySelector(".btn_paper");
+const btn_scissors = document.querySelector(".btn_scissors");
+
+btn_rock.addEventListener("click", () => {
+  playRound("rock", getComputerChoice());
+});
+btn_paper.addEventListener("click", () => {
+  playRound("paper", getComputerChoice());
+});
+btn_scissors.addEventListener("click", () => {
+  playRound("scissors", getComputerChoice());
+});
+
 function getComputerChoice() {
   let choices = ["rock", "paper", "scissors"];
   return choices[Math.floor(Math.random() * choices.length)];
@@ -23,57 +37,42 @@ function getHumanChoice() {
   }
 }
 
-function playGame(number) {
-  let humanSelection = getHumanChoice();
-  let computerSelection = getComputerChoice();
+function playRound(humanChoice, computerChoice) {
   let humanScore = 0;
   let computerScore = 0;
 
-  function playRound(humanChoice, computerChoice) {
-    if (humanChoice === "rock" && computerChoice === "rock") {
-      console.log("It's a draw! Rock vs Rock");
-      console.log(`Scores: You: ${humanScore} Computer: ${computerScore}`);
-    } else if (humanChoice === "rock" && computerChoice === "paper") {
-      console.log("You lose! Paper beats Rock.");
-      computerScore++;
-      console.log(`Scores: You: ${humanScore} Computer: ${computerScore}`);
-    } else if (humanChoice === "rock" && computerChoice === "scissors") {
-      console.log("You win! Rock beats Scissors");
-      humanScore++;
-      console.log(`Scores: You: ${humanScore} Computer: ${computerScore}`);
-    } else if (humanChoice === "paper" && computerChoice === "rock") {
-      console.log("You win! Paper beats Rock!");
-      humanScore++;
-      console.log(`Scores: You: ${humanScore} Computer: ${computerScore}`);
-    } else if (humanChoice === "paper" && computerChoice === "paper") {
-      console.log("It's a draw! Paper vs Paper");
-      console.log(`Scores: You: ${humanScore} Computer: ${computerScore}`);
-    } else if (humanChoice === "paper" && computerChoice === "scissors") {
-      console.log("You lose! Scissors beat Paper.");
-      computerScore++;
-      console.log(`Scores: You: ${humanScore} Computer: ${computerScore}`);
-    } else if (humanChoice === "scissors" && computerChoice === "rock") {
-      console.log("You lose! Rock beats Paper.");
-      computerScore++;
-      console.log(`Scores: You: ${humanScore} Computer: ${computerScore}`);
-    } else if (humanChoice === "scissors" && computerChoice === "paper") {
-      console.log("You win! Scissors beat Paper.");
-      humanScore++;
-      console.log(`Scores: You: ${humanScore} Computer: ${computerScore}`);
-    } else {
-      console.log("It's a draw! Scissors vs Scissors");
-      console.log(`Scores: You: ${humanScore} Computer: ${computerScore}`);
-    }
-  }
-
-  if (humanScore > computerScore) {
-    console.log("You won!");
-    console.log(`Final Scores: You: ${humanScore} Computer: ${computerScore}`);
-  } else if (humanScore < computerScore) {
-    console.log("You lost!");
-    console.log(`Final Scores: You: ${humanScore} Computer: ${computerScore}`);
+  if (humanChoice === "rock" && computerChoice === "rock") {
+    console.log("It's a draw! Rock vs Rock");
+    console.log(`Scores: You: ${humanScore} Computer: ${computerScore}`);
+  } else if (humanChoice === "rock" && computerChoice === "paper") {
+    console.log("You lose! Paper beats Rock.");
+    computerScore++;
+    console.log(`Scores: You: ${humanScore} Computer: ${computerScore}`);
+  } else if (humanChoice === "rock" && computerChoice === "scissors") {
+    console.log("You win! Rock beats Scissors");
+    humanScore++;
+    console.log(`Scores: You: ${humanScore} Computer: ${computerScore}`);
+  } else if (humanChoice === "paper" && computerChoice === "rock") {
+    console.log("You win! Paper beats Rock!");
+    humanScore++;
+    console.log(`Scores: You: ${humanScore} Computer: ${computerScore}`);
+  } else if (humanChoice === "paper" && computerChoice === "paper") {
+    console.log("It's a draw! Paper vs Paper");
+    console.log(`Scores: You: ${humanScore} Computer: ${computerScore}`);
+  } else if (humanChoice === "paper" && computerChoice === "scissors") {
+    console.log("You lose! Scissors beat Paper.");
+    computerScore++;
+    console.log(`Scores: You: ${humanScore} Computer: ${computerScore}`);
+  } else if (humanChoice === "scissors" && computerChoice === "rock") {
+    console.log("You lose! Rock beats Paper.");
+    computerScore++;
+    console.log(`Scores: You: ${humanScore} Computer: ${computerScore}`);
+  } else if (humanChoice === "scissors" && computerChoice === "paper") {
+    console.log("You win! Scissors beat Paper.");
+    humanScore++;
+    console.log(`Scores: You: ${humanScore} Computer: ${computerScore}`);
   } else {
-    console.log("Nobody wins!");
-    console.log(`Final Scores: You: ${humanScore} Computer: ${computerScore}`);
+    console.log("It's a draw! Scissors vs Scissors");
+    console.log(`Scores: You: ${humanScore} Computer: ${computerScore}`);
   }
 }
